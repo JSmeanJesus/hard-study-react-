76983809c3f5c264b9541da55b5a531f68e19ea5
Nick <tenebrissfer@gmail.com>
2023-11-09 14:58:24 +0200

    fix test

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,12 +1,15 @@
 import { render } from "@testing-library/react";
 import  { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
+import { BrowserRouter } from "react-router-dom";
 import i18nForTests from "shared/config/i18n/i18nForTests";
 
 export function renderWithTranslation(component: ReactNode) {
     return render(
-        <I18nextProvider i18n={i18nForTests}>
-            {component}
-        </I18nextProvider>,
+        <BrowserRouter>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+        </BrowserRouter>,
     )
 }
