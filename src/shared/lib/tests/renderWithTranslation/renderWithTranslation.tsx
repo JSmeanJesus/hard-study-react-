1b5e6aec0fcbc625ce4b62815fe9adbce9ed685b
Nick <tenebrissfer@gmail.com>
2023-11-09 15:14:42 +0200

    fix sidebar test

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,15 +1,27 @@
 import { render } from "@testing-library/react";
 import  { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
-import { BrowserRouter } from "react-router-dom";
+import {  MemoryRouter } from "react-router-dom";
 import i18nForTests from "shared/config/i18n/i18nForTests";
 
-export function renderWithTranslation(component: ReactNode) {
+
+export interface renderWithTranslation {
+    route?: string;
+}
+
+export function renderWithTranslation(
+    component: ReactNode,
+    options: renderWithTranslation = {}) {
+
+    const {
+        route = '/',
+    } = options;
+
     return render(
-        <BrowserRouter>
+        <MemoryRouter initialEntries={[route]}>
             <I18nextProvider i18n={i18nForTests}>
                 {component}
             </I18nextProvider>
-        </BrowserRouter>,
+        </MemoryRouter>,
     )
 }
